8eb77c99ef722ef3c163c18c34ca299366f32649
thisisAdityaRajGupta <161523222+thisisAdityaRajGupta@users.noreply.github.com>
2024-03-14 11:56:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function goToNextQuestion() {
+    window.location.href = "question1.html"; // Replace with appropriate question page URL
+}
+function goToNextQuestion1() {
+    window.location.href = "question2.html"; // Replace with appropriate question page URL
+}
+function goToNextQuestion2() {
+    window.location.href = "question3.html"; // Replace with appropriate question page URL
+}
+function showApologyPage() {
+    window.location.href = "apology.html"; // Replace with apology page URL
+}
+
+function goToThankYouPage() {
+    window.location.href = "thankyou.html"; // Replace with thank you page URL
+}
+function showApologyPage() {
+    window.location.href = "no.html"; // Redirect to "Please select option 1" page
+}
